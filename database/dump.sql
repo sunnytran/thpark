9f50b6e2ddedbc2be09daf8dcbbd51d9d50327f3
Andrew Vieira <5641660+AndrewVieira@users.noreply.github.com>
2020-02-26 20:02:05 -0600

    First Exported Database SQL File

---
+++ database/dump.sql
@@ -0,0 +1,47 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.2
+-- Dumped by pg_dump version 12.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: test; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.test (
+    id integer,
+    value character varying(16)
+);
+
+
+ALTER TABLE public.test OWNER TO postgres;
+
+--
+-- Data for Name: test; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.test (id, value) FROM stdin;
+123	Joe
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+

52d06c98121bdbd8bb25be775b61621992f48c18
Andrew Vieira <5641660+AndrewVieira@users.noreply.github.com>
2020-03-14 15:57:58 -0500

    Finished adding tuples to all tables

--- database/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-	account_id uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-	username text NOT NULL,
-	password text NOT NULL
-);
-
-INSERT INTO users(username, password) VALUES ('test', '12345');

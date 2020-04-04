23dce6223506dd9087e718621e3fe70c1a594d4e
Andrew Vieira <5641660+AndrewVieira@users.noreply.github.com>
2020-04-04 08:19:26 -0500

    Staff is now listed on Frontend

--- pages/staff.js
+++ pages/staff.js
@@ -1,13 +1,44 @@
 
 import Layout from '../components/Layout';
 
-const Staff = (props) => (
-	<Layout>
-		<div>
-			<h1>Staff</h1>
-		</div>
-	</Layout>
-);
+class Staff extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			staff: []
+		}
+	}
+
+	componentDidMount(){
+		fetch("http://localhost:3000/api/staff")
+		.then(res => res.json())
+		.then (
+			(result)=> {
+				this.setState({
+					staff: result
+				});
+				console.log(result);
+			}
+		)
+	}
+
+	render() {
+		const staff = this.state.staff;
+
+		return (
+			<Layout>
+				<div>
+					<h1>Staff</h1>
+					<ul>
+						{staff.map(s => {
+							return (<li key={s.first_name}>{s.first_name} {s.last_name}</li>);
+						})}
+					</ul>
+				</div>
+			</Layout>
+		)
+	}
+};
 
 export default Staff;
 

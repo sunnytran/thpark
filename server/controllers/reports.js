df5dfdf677bf127ce12efbedecfbd8a6c2fcfc7a
Andrew Vieira <5641660+AndrewVieira@users.noreply.github.com>
2020-04-07 23:23:14 -0500

    Fixed visitor report bug

--- server/controllers/reports.js
+++ server/controllers/reports.js
@@ -1,17 +1,18 @@
 const handleReportsGet = (db) => (req, res) => {
 	const values = {report: req.body.report, days: req.body.days};
 	if (req.body.report === "visitors"){
-		db.query('select CURRENT_DATE - ${days} + d, count(s.timestamp::date = CURRENT_DATE-${days}+d AND sale_type=\'ticket\') as visitor_count FROM generate_series(1, ${days}) d LEFT JOIN sale s ON s.timestamp::date = CURRENT_DATE - ${days} + d AND sale_type=\'ticket\' GROUP by d ORDER by d', values)
+		db.query('select CURRENT_DATE - ${days} + d AS date, count(s.timestamp::date = CURRENT_DATE-${days}+d AND sale_type=\'ticket\') as visitor_count FROM generate_series(1, ${days}) d LEFT JOIN sale s ON s.timestamp::date = CURRENT_DATE - ${days} + d AND sale_type=\'ticket\' GROUP by d ORDER by d', values)
 		.then(function(data) {
 			var sum = 0;
-			var count = 0;
 
 			for (var key in data){
-				count = count + 1;
-				sum = sum + data[key];
+				//console.log(key);
+				sum = sum + parseInt(data[key].visitor_count);
 			}
+			//console.log(sum);
 
-			var average = {"average" : sum / count};
+			var average = {"average" : sum / req.body.days};
+			//console.log(average);
 
 			var pack = [];
 			pack.push(data);

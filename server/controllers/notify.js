501a8aa26dab6516cc8f8d6c46c0190a02338331
Andrew Vieira <5641660+AndrewVieira@users.noreply.github.com>
2020-04-18 11:32:18 -0500

    Notification Route

--- server/controllers/notify.js
+++ server/controllers/notify.js
@@ -1,4 +1,12 @@
 const handleRidesOnGet = (notifications) => (req, res) => {
+	let data = [];
+
+
+	if (notifications.length !== 0){
+		data = notifications;
+	}
+
+	res.json(data);
 }
 
 module.exports = {

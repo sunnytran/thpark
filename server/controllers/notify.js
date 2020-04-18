daf3ba38057264acf5642d52474dca5d6873d5b7
Andrew Vieira <5641660+AndrewVieira@users.noreply.github.com>
2020-04-18 13:16:23 -0500

    Another notice fix

--- server/controllers/notify.js
+++ server/controllers/notify.js
@@ -1,13 +1,13 @@
-var old_notifications = [];
+var old_notifications = "";
 
 const handleNotifyGet = (notifications) => async (req, res) => {
 	let data = [];
 
-	if (notifications.length > 0 && await JSON.stringify(notifications) !== await JSON.stringify(old_notifications)){
+	if (notifications.length > 0 && await JSON.stringify(notifications) !== old_notifications){
 		data = notifications;
 	}
 
-	old_notifications = notifications;
+	old_notifications = JSON.stringify(notifications);
 
 	console.log("NOTIFICATION DATA: " + data);
 

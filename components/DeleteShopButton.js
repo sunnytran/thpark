be5bcf715b56fe6e7e17c43500ce00c2fe41e1e2
William Lam <43024206+weeyumb@users.noreply.github.com>
2020-04-21 19:36:56 -0500

    Add files via upload

---
+++ components/DeleteShopButton.js
@@ -0,0 +1,52 @@
+import Popup from '../components/Popup';
+
+class DeleteShopButton extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			showPop: false
+		}
+
+		this.togglePop = this.togglePop.bind(this);
+		this.edit = this.edit.bind(this);
+
+		this.inputNewName = React.createRef();
+		this.inputNewLocation = React.createRef();
+	}
+
+	togglePop() {
+		this.setState((prev, props) = > {
+			const newPop = !prev.showPop;
+
+			return { showPop: newPop };
+		});
+	}
+
+	async removeStore(i) {
+		const res = await fetch("https://www.tpmanagement.app/api/shops", {
+			method: 'DELETE',
+			headers : {'Content-Type': 'application/json; charset=utf-8'},
+			body : JSON.stringify({"name": i.shop_name})
+		});
+		this.getSetup();
+	};
+
+	render() {
+		const shop = this.props.shop;
+		return(
+			<div>
+				< button class = "button is-small" onClick = { this.togglePop } >
+					<span class = "icon">
+							<i class = "fa fa-times">< / i>
+					< / span>
+				< / button>
+
+				< Popup closePopup = { this.togglePop } showPop = { this.state.showPop } title = "Delete Confirmation" submitText = "Delete" btnFunc = { this.removeStore(i) } >
+					<div class = "field">
+						<label class="label"> Are you sure you want to delete this shop? < /label>
+					< / div>
+				< / Popup>
+			< / div>
+		);
+	}
+};
